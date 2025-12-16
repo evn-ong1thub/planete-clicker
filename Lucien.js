cca2bb6e511628ea1afe0b775b657fd7347caa5b
Lucien <avezardlucien@gmail.com>
2025-12-16 17:10:40 +0100

    j'ai utilisé state

--- Lucien.js
+++ Lucien.js
@@ -1,59 +1,71 @@
+var clickCountStone = facile.state(0);
 
+facile.onClick('#Planet-Stone', () => {
+    clickCountStone.value = clickCountStone.value + 1
+})
+clickCountStone.onChange((newCount) => {
+    facile.write('#countStone', newCount);
+});
 
-        var clickCountStone = 0;
-
-        facile.onClick('#Planet-Stone',() => {
-            clickCountStone = clickCountStone + 1
-            facile.write('#countStone', clickCountStone)
-        })
-        
-        var clickCountWood  = 0;
+var clickCountWood = facile.state(0);
 facile.hide('#Planet-Wood')
-        facile.onClick('#Planet-Wood',() => {
-            clickCountWood = clickCountWood + 1
-            facile.write('#countWood', clickCountwood)
-        })
+facile.onClick('#Planet-Wood', () => {
+    clickCountWood.value = clickCountWood.value + 1
+})
+clickCountWood.onChange((newCount) => {
+    facile.write('#countWood', newCount);
+})
 
-        var clickCountWater = 0
+var clickCountWater = facile.state(0);
 facile.hide('#Planet-Water')
-        facile.onClick('#Planet-Water',() => {
-            clickCountWater = clickCountWater + 1
-            facile.write('#countWater', clickCountWater)
-        })
+facile.onClick('#Planet-Water', () => {
+    clickCountWater.value = clickCountWater.value + 1
+})
+clickCountWater.onChange((newCount) => {
+    facile.write('#countWater', newCount)
+})
 
-        var clickCountCoal = 0
+var clickCountCoal = facile.state(0)
 facile.hide('#Planet-Coal')
-        facile.onClick('#Planet-Coal',() => {
-            clickCountCoal = clickCountCoal + 1
-            facile.write('#countCoal', clickCountCoal)
-        })
+facile.onClick('#Planet-Coal', () => {
+    clickCountCoal.value = clickCountCoal.value + 1
+})
+clickCountCoal.onChange((newCount) => {
+    facile.write('#countCoal', newCount)
+})
 
-        var clickCountIron = 0
+var clickCountIron = facile.state(0)
 facile.hide('#Planet-Iron')
-        facile.onClick('#Planet-Iron',() => {
-            clickCountIron = clickCountIron + 1
-            facile.write('#countIron', clickCountIron)
-        })
+facile.onClick('#Planet-Iron', () => {
+    clickCountIron.value = clickCountIron.value + 1
+})
+clickCountIron.onChange((newCount) => {
+    facile.write('#countIron', newCount)
+})
 
-        var clickCountUranium = 0
+var clickCountUranium = facile.state(0)
 facile.hide('#Planet-Uranium')
-        facile.onClick('#Planet-Uranium',() => {
-           clickCountUranium = clickCountUranium + 1 
-           facile.write('#countUranium', clickCountUranium)
-        })
+facile.onClick('#Planet-Uranium', () => {
+    clickCountUranium.value = clickCountUranium.value + 1
+})
+clickCountUranium.onChange((newCount) => {
+    facile.write('#countUranium', newCount)
+})
 
-        var clickCountObsidian = 0
+var clickCountObsidian = facile.state(0)
 facile.hide('#Planet-Obsidian')
-        facile.onClick('#Planet-Obsidian',() => {
-            clickCountObsidian = clickCountObsidian + 1
-            facile.write('#countObsidian', clickCountObsidian)
-        })
+facile.onClick('#Planet-Obsidian', () => {
+    clickCountObsidian.value = clickCountObsidian.value + 1
+})
+clickCountObsidian.onChange((newCount) => {
+    facile.write('#countObsidienne', newCount)
+})
 
-        var clickCountDiamond = 0
+var clickCountDiamond = facile.state(0)
 facile.hide('#Planet-Diamond')
-        facile.onClick('#Planet-Diamond',() => {
-            clickCountDiamant = clickCountDiamond + 1
-            facile.write('#countDiamond', clickCountDiamond)
-        })
-
-    
+facile.onClick('#Planet-Diamond', () => {
+    clickCountDiamant.value = clickCountDiamond.value + 1
+})
+clickCountDiamond.onChange((newCount) => {
+    facile.write('#countDiamond', newCount)
+})
